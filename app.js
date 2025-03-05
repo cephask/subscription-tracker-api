0460b950558d54a784fbe4e0723f085126838562
Cephas <cephaskiprono@gmail.com>
2025-03-06 00:35:10 +0300

    config update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+import { PORT } from './config/env.js'
 
 const app = express();
 
@@ -7,8 +8,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(3000, () => {
-  console.log("Subscription Tracker API is running on port 3000!");
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API is running on port ${PORT}!`);
 });
 
 export default app;
